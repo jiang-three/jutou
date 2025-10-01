9d0bdad3c6ceaa2bf48ee2088abbd70f648112bc
jiang <730768118@qq.com>
2025-10-01 23:13:48 +0800

    终版

--- accounts/apps.py
+++ accounts/apps.py
@@ -5,14 +5,3 @@ class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
 
-from django.db.models.signals import post_migrate
-
-def create_superuser(sender, **kwargs):
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@example.com', 'jiang44811957')
-
-class YourAppConfig(AppConfig):
-    def ready(self):
-        post_migrate.connect(create_superuser, sender=self)
